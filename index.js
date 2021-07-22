0ea5636ffeaf53ebd6de4cfa015a3dc2beb3addc
Dana Walsh <dana.e.walsh@gmail.com>
2021-07-22 12:46:05 -0700

    Basic functionality complete.

--- index.js
+++ index.js
@@ -17,11 +17,39 @@ const orderAccounting = async function (orderNumber) {
 
   let taxRateResponse = await helper.apiRequests.taxRate(orderInfo.zip_code);
   let tax = await taxRateResponse.json();
-  console.log('tax', tax)
+
+  // build place holder result
+  let accountingDetails = {
+    'Order': orderNumber,
+    'Customer Name': orderInfo.shipping_name,
+    'Subtotal' : 0,
+    'Taxes': 0,
+    'Total':0
+  }
 
   // iterate through order_items
     // check quantity
     // check taxable
+  orderInfo.order_items.forEach((item) => {
+    let price = item.price / 100;
+    let quantity = item.quantity;
+    let taxable = item.taxable;
+
+    let totalAmountNoTax = price * quantity;
+
+    accountingDetails.Subtotal += totalAmountNoTax;
+
+    if (taxable) {
+      let totalTax = totalAmountNoTax * (tax.tax_rate / 100);
+      let roundedTotalTax = Math.round(100 * totalTax)/100;
+      accountingDetails.Taxes += roundedTotalTax;
+    }
+  })
+
+  accountingDetails.Total = Math.round(100 * (accountingDetails.Subtotal + accountingDetails.Taxes))/100;
+
+  console.log(accountingDetails);
+  return accountingDetails;
 }
 
 orderAccounting('sfg47');
